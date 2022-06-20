2647fd5b94c7ec7b58081be6a00e39f2c2793a09
Caio Gomes <caiogame100@gmail.com>
2022-06-19 21:38:33 -0300

    add home page

--- front-end/src/pages/home/index.tsx
+++ front-end/src/pages/home/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import './styles.css';
+
+import Canva from '../../components/canva';
+import AddImage from '../../components/add-images';
 
 const App: React.FC = () => {
   return (
-    <div>
-      Hello pessoas
-      <></>
+    <div className='container'>
+      <h1 className='my-gif'>My Gif</h1>
+      <Canva />
+      <AddImage />
     </div>
   );
 };
