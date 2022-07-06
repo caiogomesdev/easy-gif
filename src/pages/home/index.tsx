ce16f10d65b7196cb35e11aa795a8b0dab08fd8e
Caio Gomes <caiogame100@gmail.com>
2022-07-05 22:30:27 -0300

    feat: change style home page to styled-components

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import './styles.css';
+import { Container, MyGif } from './styles';
 
 import Canva from '../../components/canva';
 import AddImage from '../../components/add-images';
 
 const App: React.FC = () => {
   return (
-    <div className='container'>
-      <h1 className='my-gif'>My Gif</h1>
+    <Container>
+      <MyGif>My Gif</MyGif>
       <Canva />
       <AddImage />
-    </div>
+    </Container>
   );
 };
 
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,13 +1,15 @@
-.container {
+import styled from 'styled-components';
+
+export const Container = styled.div`
   height: 100vh;
   width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-}
+`
 
-.my-gif {
+export const MyGif = styled.h1`
   margin-bottom: 10px;
   position: relative;
-}
+`
