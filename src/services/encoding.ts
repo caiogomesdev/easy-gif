8c429b4e74a1f396d160e1b8ada750c42a8f2c56
Caio Gomes <caiogame100@gmail.com>
2022-07-05 22:25:24 -0300

    init class encoding

--- src/services/encoding.ts
+++ src/services/encoding.ts
@@ -5,9 +5,6 @@ export class Encoding {
   constructor(private gitEncoder: GifEncoder){
     this.gitEncoder.start();
   }
-  addImage(): void {
-
-  }
   finish(): Buffer {
     return this.gitEncoder.out.getData();
   }
