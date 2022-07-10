1cf14d1de22d660e0b1f0ec3cdf87da2b54adfe5
Caio Gomes <caiogame100@gmail.com>
2022-07-10 16:55:36 -0300

    add removeFrame on store

--- src/store/image-store.tsx
+++ src/store/image-store.tsx
@@ -1,28 +1,43 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Frame } from "../components/contracts";
 
 const store = createSlice({
   name: 'image',
   initialState: {
-    data: [],
+    data: [] as Frame[],
     currentIndex: 0,
+    length: 0,
   },
   reducers: {
-    addImage(state: IState, context: PayloadAction<string>){
+    addFrame(state: IState, context: PayloadAction<Frame>){
       const index = state.data.length;
       state.data = [...state.data, context.payload]
       state.currentIndex = index;
+      state.length = state.data.length;
     },
-    changeImage(state: IState, context: PayloadAction<number>){
+    changeFrame(state: IState, context: PayloadAction<number>){
       state.currentIndex = context.payload
-      console.log('mudou',context.payload)
     },
+    removeFrame(state: IState, context: PayloadAction<number>){
+      const index = context.payload;
+      state.data.splice(index, 1);
+      state.length = state.data.length;
+      let currentIndex = state.length;
+      if (index === currentIndex){
+        currentIndex--;
+      }else {
+        currentIndex = index;
+      }
+      state.currentIndex = currentIndex;
+    }
   },
 })
 
 type IState = {
-  data: string[],
+  data: Frame[],
   currentIndex: number,
+  length: number,
 }
 
-export const { addImage, changeImage } = store.actions;
+export const { addFrame, changeFrame, removeFrame } = store.actions;
 export default store.reducer;
