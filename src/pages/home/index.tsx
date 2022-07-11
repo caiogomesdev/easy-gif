55c5c3bae71d724319980534d7e8c24a7afa0ff9
Caio Gomes <caiogame100@gmail.com>
2022-07-11 00:34:41 -0300

    home adjustments

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Container, MyGif } from './styles';
 
 import Canva from '../../components/canva';
 import AddImage from '../../components/add-images';
+import { rootState } from '../../store';
 
 const App: React.FC = () => {
+  const images = useSelector((state: rootState ) => state.image);
+
   return (
-    <Container>
+    <Container imagesLenght={images.data.length}>
       <MyGif>My Gif</MyGif>
       <Canva />
       <AddImage />
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,12 +1,16 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+interface IContainer{
+  imagesLenght: number;
+}
+export const Container = styled.div<IContainer>`
   height: 100vh;
   width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: ${props => props.imagesLenght ? '' : 'center'};
+  margin-top: ${props => props.imagesLenght ? '5' : '0'}rem;
 `
 
 export const MyGif = styled.h1`
